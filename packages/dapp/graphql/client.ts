b9702fbc80ed62ee0518c025167ecb6442a61106
dan13ram <dan13ram@gmail.com>
2024-09-04 09:01:34 +0530

    frontend on sepolia

--- packages/dapp/graphql/client.ts
+++ packages/dapp/graphql/client.ts
@@ -16,42 +16,13 @@ const GRAPH_API_KEY =
   process.env.GRAPH_API_KEY;
 
 export const SUPPORTED_NETWORK_INFO: NetworkInfo = {
-  // '0xa': {
-  //   chainId: '0xa',
-  //   prodSubgraphUrl: `https://gateway-arbitrum.network.thegraph.com/api/${GRAPH_API_KEY}/subgraphs/id/9GTxhTXaiJqgYGuSsMjH3KWdEQfFf8BzrTQKWNKZBMzB`,
-  //   devSubgraphUrl:
-  //     'https://api.studio.thegraph.com/query/71457/quest-chains-optimism/version/latest',
-  // },
-  // '0x64': {
-  //   chainId: '0x64',
-  //   prodSubgraphUrl: `https://gateway-arbitrum.network.thegraph.com/api/${GRAPH_API_KEY}/subgraphs/id/EdGKgTZKUfZnWmG3rPAirBis6xUJpAJ8dgvMts9Aiixb`,
-  //   devSubgraphUrl:
-  //     'https://api.studio.thegraph.com/query/71457/quest-chains-gnosis/version/latest',
-  // },
-  // '0x89': {
-  //   chainId: '0x89',
-  //   prodSubgraphUrl: `https://gateway-arbitrum.network.thegraph.com/api/${GRAPH_API_KEY}/subgraphs/id/29xgvK1yyJkcq3zM1wqTvFh3riMzpTFjLqu3PYZ1ZSv5`,
-  //   devSubgraphUrl:
-  //     'https://api.studio.thegraph.com/query/71457/quest-chains-polygon/version/latest',
-  // },
-  // '0xa4b1': {
-  //   chainId: '0xa4b1',
-  //   prodSubgraphUrl: `https://gateway-arbitrum.network.thegraph.com/api/${GRAPH_API_KEY}/subgraphs/id/J3V2pB7DFYaMVNyU7GbrQNsiptNfuk9MZ3GNjLHYmcpD`,
-  //   devSubgraphUrl:
-  //     'https://api.studio.thegraph.com/query/71457/quest-chains-arbitrum/version/latest',
-  // },
-  // '0x4268': {
-  //   chainId: '0x4268',
-  //   prodSubgraphUrl: `https://gateway-testnet-arbitrum.network.thegraph.com/api/${GRAPH_API_KEY}/subgraphs/id/9ZDv6YDBRyxPBeLvbHz8oTmjci2FTySbx8v3fko4MEso`,
-  //   devSubgraphUrl:
-  //     'https://api.studio.thegraph.com/query/71457/quest-chains-holesky/version/latest',
-  // },
-  // '0xaa36a7': {
-  //   chainId: '0xaa36a7',
-  //   prodSubgraphUrl: `https://gateway-testnet-arbitrum.network.thegraph.com/api/${GRAPH_API_KEY}/subgraphs/id/5fRc469U46WVkH9WWYQ2wUuS3cdrX14WNmHGyaqg87Fe`,
-  //   devSubgraphUrl:
-  //     'https://api.studio.thegraph.com/query/71457/quest-chains-sepolia/version/latest',
-  // },
+  11155111: {
+    chainId: 11155111,
+    prodSubgraphUrl:
+      'https://api.studio.thegraph.com/query/71457/zerotoone-sepolia/version/latest',
+    devSubgraphUrl:
+      'https://api.studio.thegraph.com/query/71457/zerotoone-sepolia/version/latest',
+  },
   31337: {
     chainId: 31337,
     prodSubgraphUrl: 'http://localhost:8000/subgraphs/name/zerotoone',
@@ -59,11 +30,16 @@ export const SUPPORTED_NETWORK_INFO: NetworkInfo = {
   },
 };
 
+const cache = new Map<SupportedChainId, Client>();
+
 export const getClient = (chainId: SupportedChainId): Client => {
   const info = SUPPORTED_NETWORK_INFO[chainId];
   if (!info) throw new Error(`Unsupported chainId: ${chainId}`);
-  return createClient({
+  if (cache.has(chainId)) return cache.get(chainId)!;
+  const newClient = createClient({
     url: GRAPH_API_KEY ? info.prodSubgraphUrl : info.devSubgraphUrl,
     exchanges: [dedupExchange, fetchExchange],
   });
+  cache.set(chainId, newClient);
+  return newClient;
 };
