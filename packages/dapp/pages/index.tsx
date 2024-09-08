cdb5abc4ad5bca5fbc2fc5667e3c87aff039d78f
dan13ram <dan13ram@gmail.com>
2024-09-08 15:52:10 +0530

    added banner image

--- packages/dapp/pages/index.tsx
+++ packages/dapp/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Button, Heading, Image, SimpleGrid, VStack } from '@chakra-ui/react';
 import Link from 'next/link';
 
 import { Page } from '@/components/Layout/Page';
@@ -8,16 +8,27 @@ const Explore: React.FC = () => {
   return (
     <Page alignItems="flex-start" gap={4} flex={1}>
       <HeadComponent />
-      <VStack w="100%">
-        <Heading>Level up with Zero to One!</Heading>
-        <Link href="/courses">
-          <Button>Courses</Button>
-        </Link>
-        <Text>OR</Text>
-        <Heading>Earn by doing Gigs</Heading>
-        <Link href="/gigs">
-          <Button>Gigs</Button>
-        </Link>
+      <VStack w="100%" spacing={16}>
+        <Image src="/banner.png" alt="banner" />
+        <SimpleGrid columns={2} w="100%" spacing={16}>
+          <VStack
+            spacing={4}
+            bg="purplePlop.400"
+            p={20}
+            justify="space-between"
+          >
+            <Heading>Level up with our Zero to One!</Heading>
+            <Link href="/courses">
+              <Button size="lg">Explore Courses</Button>
+            </Link>
+          </VStack>
+          <VStack spacing={4} bg="blue.600" p={20} justify="space-between">
+            <Heading>Earn by doing!</Heading>
+            <Link href="/gigs">
+              <Button size="lg">Browse Gigs</Button>
+            </Link>
+          </VStack>
+        </SimpleGrid>
       </VStack>
     </Page>
   );
